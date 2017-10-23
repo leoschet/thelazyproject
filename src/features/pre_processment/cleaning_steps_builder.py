4fb4f919af30a588fac125d6de7e4765c8216749
mcbsf <mcbsf@cin.ufpe.br>
2017-10-23 17:04:40 -0200

    ajeitando os regex, adicionando lemming e adaptando o main.py

--- src/features/pre_processment/cleaning_steps_builder.py
+++ src/features/pre_processment/cleaning_steps_builder.py
@@ -1,21 +1,28 @@
 import nltk
+from nltk.stem import WordNetLemmatizer
+from nltk import re
 
 # Regex for text cleaning
 NUMBER_REGEX = re.compile(r'[0-9]')
 NOT_CHAR_REGEX = re.compile(r'[^A-Za-z0-9]')
+BEGIN_NUMBER_REGEX = re.compile(r'[^0-9]')
 
 english_stopwords = nltk.corpus.stopwords.words('english')
 porter_stemmer = nltk.stem.PorterStemmer()
+wordnet_lemmatizer = WordNetLemmatizer()
 
-def cleaning_steps_builder(document_terms, remove_stopwords, do_stemming):
+def cleaning_steps_builder(document_terms, remove_stopwords = False, do_stemming = False, do_lemmatizing = False):
 	document_terms = [word.lower() for word in document_terms]
 	
-	document_terms = [NOT_CHAR_REGEX.sub('', word) for word in document_terms]
-	document_terms = ['<NUM>' for word in document_terms if word.startswith(NUMBER_REGEX)]
-	document_terms = [word for word in document_terms if word is not '']
+	#document_terms = [NOT_CHAR_REGEX.sub('', word) for word in document_terms]
+	#document_terms = ['<NUM>' for word in document_terms if re.match(BEGIN_NUMBER_REGEX, word)]
+	#document_terms = [word for word in document_terms if word is not '']
 
 	if (remove_stopwords):
 		document_terms = [word for word in document_terms if word not in english_stopwords]
+
+	if (do_lemmatizing):
+		document_terms = [wordnet_lemmatizer.lemmatize(word, pos='n') for word in document_terms]
 	
 	if (do_stemming):
 		document_terms = [porter_stemmer.stem(word) for word in document_terms]
--- src/main.py
+++ src/main.py
@@ -41,10 +41,11 @@ for doc_name in collection.train_docs:
 	corpus[doc_name] = collection.get_words(doc_name)
 	# break
 
-document_clean_function = lambda document: cleaning_steps_builder(document, remove_stopwords = False, do_stemming = False)
-document_stop_function = lambda document: cleaning_steps_builder(document, remove_stopwords = True, do_stemming = False)
-document_stem_function = lambda document: cleaning_steps_builder(document, remove_stopwords = False, do_stemming = True)
-document_stop_stem_function = lambda document: cleaning_steps_builder(document, remove_stopwords = True, do_stemming = True)
+document_clean_function = lambda document: cleaning_steps_builder(document, remove_stopwords = False, do_stemming = False, do_lemmatizing = False)
+document_stop_function = lambda document: cleaning_steps_builder(document, remove_stopwords = True, do_stemming = False, do_lemmatizing = False)
+document_stem_function = lambda document: cleaning_steps_builder(document, remove_stopwords = False, do_stemming = True, do_lemmatizing = False)
+document_stop_stem_function = lambda document: cleaning_steps_builder(document, remove_stopwords = True, do_stemming = True, do_lemmatizing = False)
+document_stop_lem_stem_function = lambda document: cleaning_steps_builder(document, remove_stopwords = True, do_stemming = True, do_lemmatizing = True)
 
 ii = InvertedIndex(corpus, collection, document_stop_function)
 ranker_stop = Ranker(ii, collection)
@@ -53,11 +54,13 @@ ranker_stop = Ranker(ii, collection)
 # ranker_stop = Ranker(InvertedIndex(corpus, document_stop_function), collection)
 # ranker_stem = Ranker(InvertedIndex(corpus, document_stem_function))
 # ranker_stop_stem = Ranker(InvertedIndex(corpus, document_stop_stem_function))
-
+print(collection.get_document(collection.test_docs[0]))
+print("\n")
 for doc in collection.test_docs:
 	rank = ranker_stop.classify(collection.get_words(doc))
-	print(rank)
-	print (collection.get_categories(doc))
-	print("\n")
+	#print(rank)
+	#print (collection.get_categories(doc))
+	#print(collection.get_words(doc))
+	#print("\n")
 
 # rank = ranker_clean.search(collection.get_words(collection.test_docs[0]))
